de305d97d748b27ce593e576ac6057c8e2dd5d91
Jenny Back <jenny@jennybacklin.se>
2024-01-12 16:47:03 +0100

    adds responsive fix

--- src/components/blocks/categories/Categories.tsx
+++ src/components/blocks/categories/Categories.tsx
@@ -2,106 +2,69 @@ import styles from './Categories.module.css';
 
 const Categories = () => {
     return (
-        <div aria-label="categories">
-            <div
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    marginLeft: '300px',
-                    marginRight: '300px'
-                }}
-            >
-                <ul
+        <div aria-label="categories" className={styles.categoriesWrapper}>
+            <ul className={styles.categoriesList}>
+                <li
                     style={{
                         display: 'flex',
-                        listStyle: 'none',
-                        justifyContent: 'space-evenly',
-                        width: '100%'
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center'
                     }}
                 >
-                    <li
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            alignItems: 'center'
-                        }}
-                    >
-                        <img
-                            alt="category-image"
-                            src="/cGJRpB59cYiaVAs1673384775.webp"
-                            style={{
-                                width: '100px',
-                                height: '100px',
-                                borderRadius: '50%',
-                                backgroundColor: 'lightgray'
-                            }}
-                        />
-                        <p>Olja & Bets</p>
-                    </li>
-                    <li
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            alignItems: 'center'
-                        }}
-                    >
-                        <img
-                            alt="category-image"
-                            src="/J7Rj4OTRLr8NJ2X1673384628.jpeg"
-                            style={{
-                                width: '100px',
-                                height: '100px',
-                                borderRadius: '50%',
-                                backgroundColor: 'lightgray'
-                            }}
-                        />
-                        <p>Väggfärg</p>
-                    </li>
-                    <li
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            alignItems: 'center'
-                        }}
-                    >
-                        <img
-                            alt="category-image"
-                            src="/1ttJcRCF2kT3bzW1673384753.jpeg"
-                            style={{
-                                width: '100px',
-                                height: '100px',
-                                borderRadius: '50%',
-                                backgroundColor: 'lightgray'
-                            }}
-                        />
-                        <p>Lack & Snickerifärg</p>
-                    </li>
-                    <li
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            justifyContent: 'center',
-                            alignItems: 'center'
-                        }}
-                    >
-                        <img
-                            alt="category-image"
-                            src="/Tv1jiskJm0FI2L41673384525.jpeg"
-                            style={{
-                                width: '100px',
-                                height: '100px',
-                                borderRadius: '50%',
-                                backgroundColor: 'lightgray',
-                                objectFit: 'cover'
-                            }}
-                        />
-                        <p>Penslar</p>
-                    </li>
-                </ul>
-            </div>
+                    <img
+                        alt="category-image"
+                        src="/cGJRpB59cYiaVAs1673384775.webp"
+                        className={styles.categoriesListImage}
+                    />
+                    <p>Olja & Bets</p>
+                </li>
+                <li
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
+                >
+                    <img
+                        alt="category-image"
+                        src="/J7Rj4OTRLr8NJ2X1673384628.jpeg"
+                        className={styles.categoriesListImage}
+                    />
+                    <p>Väggfärg</p>
+                </li>
+                <li
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
+                >
+                    <img
+                        alt="category-image"
+                        src="/1ttJcRCF2kT3bzW1673384753.jpeg"
+                        className={styles.categoriesListImage}
+                    />
+                    <p>Lack & Snickerifärg</p>
+                </li>
+                <li
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'center',
+                        alignItems: 'center'
+                    }}
+                >
+                    <img
+                        alt="category-image"
+                        src="/Tv1jiskJm0FI2L41673384525.jpeg"
+                        className={styles.categoriesListImage}
+                    />
+                    <p>Penslar</p>
+                </li>
+            </ul>
         </div>
     );
 };
