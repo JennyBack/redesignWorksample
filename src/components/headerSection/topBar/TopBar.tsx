eb2217ed5f3cfdff882aabead84ff5da1d4d2abf
Jenny Back <jenny@jennybacklin.se>
2024-01-13 13:45:14 +0100

    adjustments to navigation

--- src/components/headerSection/topBar/TopBar.tsx
+++ src/components/headerSection/topBar/TopBar.tsx
@@ -1,14 +1,28 @@
+import { TopBarItem } from '../HeaderSection';
 import styles from './TopBar.module.css';
+import CheckIcon from '@mui/icons-material/Check';
 
-const TopBar = () => {
+type TopBarProps = {
+    topBarItems: TopBarItem[];
+};
+
+const TopBar = ({ topBarItems }: TopBarProps) => {
     return (
         <div aria-label="top-bar" className={styles.wrapper}>
             <ul aria-label="top-bar-content" className={styles.list}>
-                <li>
-                    <h3 style={{ fontSize: '14px', fontWeight: 'lighter' }}>
-                        This is a sample banner text
-                    </h3>
-                </li>
+                {topBarItems.map((item) => (
+                    <li key={item.id} className={styles.listItem}>
+                        <CheckIcon
+                            sx={{
+                                height: '0.8em',
+                                width: 'auto',
+                                color: 'rgb(124, 39, 29)',
+                                paddingRight: '3px'
+                            }}
+                        />
+                        <h3 style={{ fontSize: '14px', fontWeight: 'lighter' }}>{item.title}</h3>
+                    </li>
+                ))}
             </ul>
         </div>
     );
